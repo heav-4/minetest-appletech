5d3d279b0f701e24d64a2e7dc3826897b4777aaf
Heavpoot <heavpoot@gmail.com>
2019-01-04 11:12:55 +0000

    apple pick

--- init.lua
+++ init.lua
@@ -7,6 +7,20 @@ minetest.register_node("appletech:2capple",{
 	groups={snappy=3,choppy=3,oddly_breakable_by_hand=3,flammable=3}
 })
 
+minetest.register_tool("appletech:applepick",{
+	description="Apple Pickaxe",
+	inventory_image ="atech_applepick.png",
+	tool_capabilities={
+		full_punch_interval=1.2,
+		max_drop_level=0,
+		groupcaps={
+			cracky={times={[3]=1.60*9},uses=10,maxlevel=1},
+		},
+		damage_groups = {fleshy=2},
+	},
+	groups = {flammable = 2},
+	sound = {breaks = "default_tool_breaks"},
+})
 minetest.register_craft({
 	output="appletech:capple",
 	recipe={{"default:apple","default:apple","default:apple"},{"default:apple","default:apple","default:apple"},{"default:apple","default:apple","default:apple"}}
@@ -25,3 +39,8 @@ minetest.register_craft({
 	output="appletech:2capple",
 	recipe={{"appletech:capple","appletech:capple","appletech:capple"},{"appletech:capple","appletech:capple","appletech:capple"},{"appletech:capple","appletech:capple","appletech:capple"}}
 })
+minetest.register_craft({
+	output="appletech:applepick",
+	recipe={{"appletech:capple","appletech:capple","appletech:capple"},{"","default:apple",""},{"","default:apple",""}}
+})
+
